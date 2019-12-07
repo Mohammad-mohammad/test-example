526734dc914597e56d3f8fe4244f18194699bb12
Mohammad Mohammad <it.msm88@gmail.com>
2019-12-07 23:15:31 +0100

    update quad tree file

--- quad_tree.erl
+++ quad_tree.erl
@@ -6,37 +6,22 @@
 
 -type error() :: {error, Reason :: atom()}.
 
-new(Bounds) ->
-        case validate_bounds(Bounds) of
-          true ->
-            {ok, #qtree_node { point = get_center(Bounds),
-                               bounds = Bounds,
-                               nodes = [],
-                               val = undefined }};
-          false ->
-            {error, not_power_of_2}
-        end.
+
 		
+
 validate_bounds({{X1, Y1}, {X2, Y2}}) ->
         W = erlang:abs(X2 - X1),
         H = erlang:abs(Y2 - Y1),
         W =:= H andalso
-        case W =:= H of
-          true ->
-            case (W band (W - 1)) of
-              0 ->
-                case (H band (H - 1)) of
-                  0 ->
-                    {ok, valid};
-                  _ ->
-                    {error, height_not_power_of_2}
-                end;
-              _ ->
-                {error, width_not_power_of_2}
-            end;
-          false ->
-            {error, sides_not_equal}
-        end.
+        (W band (W - 1)) =:= 0 andalso
+        (H band (H - 1)) =:= 0.
 
 get_center({{X1, Y1}, {X2, Y2}}) ->
         {erlang:trunc((X2 - X1) / 2), erlang:trunc((Y2 - Y1) / 2)}.
+		
+
+is_in_rect(BoundingRect, Point) ->
+        {{X1, Y1}, {X2, Y2}} = BoundingRect,
+        {X, Y} = Point,
+        X >= X1 andalso X =< X2 andalso
+        Y >= Y1 andalso Y =< Y2.
